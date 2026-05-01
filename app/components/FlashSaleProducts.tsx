ae0aade48e104bde8ce8681e80265005cf61f28d
unknown <lesterodra@gmail.com>
2026-05-01 16:38:03 +0800

    display validity date

--- app/components/FlashSaleProducts.tsx
+++ app/components/FlashSaleProducts.tsx
@@ -2,9 +2,22 @@
 
 import { createOrder, fetchFlashSales } from "@/lib/fetcher";
 import { FlashSale } from "../types";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import MyOrders from "./MyOrders";
+
+const formatDateAndTime = (rawDate: string): string => {
+  const date = new Date(rawDate);
+  const formatted = date.toLocaleString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
+  });
+
+  return formatted;
+};
 
 export default function AvailableProducts(props: { flashSales: FlashSale[] }) {
   const { flashSales } = props;
@@ -17,6 +30,7 @@ export default function AvailableProducts(props: { flashSales: FlashSale[] }) {
     queryFn: fetchFlashSales,
     initialData: flashSales,
   });
+  const queryClient = useQueryClient();
 
   const {
     mutate,
@@ -40,13 +54,20 @@ export default function AvailableProducts(props: { flashSales: FlashSale[] }) {
       return;
     }
     try {
-      mutate({
+      await mutate({
         email,
         product_id: 1,
         flash_sale_id: 1,
       });
 
-      refetch();
+      // Add some delay in refetching
+      setTimeout(() => {
+        // Refetch the flash sale items to reflect the actual quantity
+        refetch();
+
+        // Manual refetch user orders after successful transaction
+        queryClient.invalidateQueries({ queryKey: ["userOrders"] });
+      }, 1000);
     } catch (error) {
       console.log(error);
     }
@@ -71,6 +92,11 @@ export default function AvailableProducts(props: { flashSales: FlashSale[] }) {
               <div className="flex flex-col items-end">
                 <p className="text-md">{flashSale.product.name}</p>
                 <p className="text-xs">{flashSale.sale_quantity} available</p>
+                <p className="text-xs">
+                  (from {formatDateAndTime(flashSale.start_timestamp)}{" "}
+                  {"until "}
+                  {formatDateAndTime(flashSale.end_timestamp)})
+                </p>
               </div>
             </div>
             <p className="text-red-500">{errorMessage}</p>
@@ -79,7 +105,11 @@ export default function AvailableProducts(props: { flashSales: FlashSale[] }) {
           <div className="mt-4">
             <button
               onClick={() => handleBuyNow()}
-              disabled={isPending || flashSale.sale_quantity === 0}
+              disabled={
+                isPending ||
+                flashSale.sale_quantity === 0 ||
+                flashSale.status === "ENDED"
+              }
               className="px-4 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 disabled:bg-gray-400 w-full"
             >
               Buy Now!
--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black">
         <p className="text-2xl font-bold">Flash Sale ! ! !</p>
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left mt-20">
+        <div className="flex flex-col items-center gap-6 text-center sm:text-left mt-20">
           <AvailableProducts flashSales={flashSales} />
         </div>
       </main>
